5d05d9e52a3f1a4e05bb95f3f6eb936c7a2f6ca2
Cecchy2 <dariocecchinato@gmail.com>
2024-10-09 13:52:53 +0200

    spostato metodo get su authorization per ricevere le ricette allávvio senza autorizazione

--- src/main/java/dariocecchinato/capstone_sicily_fresh/controllers/AuthorizationController.java
+++ src/main/java/dariocecchinato/capstone_sicily_fresh/controllers/AuthorizationController.java
@@ -1,5 +1,6 @@
 package dariocecchinato.capstone_sicily_fresh.controllers;
 
+import dariocecchinato.capstone_sicily_fresh.entities.Ricetta;
 import dariocecchinato.capstone_sicily_fresh.entities.Utente;
 import dariocecchinato.capstone_sicily_fresh.enums.RuoloUtente;
 import dariocecchinato.capstone_sicily_fresh.exceptions.BadRequestException;
@@ -8,9 +9,12 @@ import dariocecchinato.capstone_sicily_fresh.payloads.UtenteLoginResponseDTO;
 import dariocecchinato.capstone_sicily_fresh.payloads.UtentiPayloadDTO;
 import dariocecchinato.capstone_sicily_fresh.payloads.UtentiResponseDTO;
 import dariocecchinato.capstone_sicily_fresh.services.AuthorizationsService;
+import dariocecchinato.capstone_sicily_fresh.services.RicetteService;
 import dariocecchinato.capstone_sicily_fresh.services.UtentiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -27,6 +31,8 @@ public class AuthorizationController {
     private UtentiService utentiService;
     @Autowired
     private AuthorizationsService authorizationsService;
+    @Autowired
+    private RicetteService ricetteService;
 
     @PostMapping("/login")
     public UtenteLoginResponseDTO login(@RequestBody UtenteLoginDTO body){
@@ -53,5 +59,18 @@ public class AuthorizationController {
         
         Utente newUser = utentiService.saveUtente(body, avatar);
         return new UtentiResponseDTO(newUser.getId());
+    }
+
+    @GetMapping
+    public Page<Ricetta> findAll (@RequestParam(defaultValue = "0") int page,
+                                  @RequestParam(defaultValue = "10") int size,
+                                  @RequestParam(defaultValue = "id") String sortby){
+        return ricetteService.findAll(page, size, sortby);
+    }
+
+    @GetMapping("/{ricettaId}")
+    public Ricetta findById (@PathVariable UUID ricettaId){
+        Ricetta found = this.ricetteService.findById(ricettaId);
+        return found;
     }
 }
